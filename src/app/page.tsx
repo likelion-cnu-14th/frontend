3a3f681792dc6e2c459f51fcbacba25d0b412ac4
성채원 <julia2003a@naver.com>
2026-03-25 15:51:15 +0900

    7차시 정적자원 사용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
       <>
       <h2>Welcome!</h2>
-      Hello, WEB!</>
+      Hello, WEB!
+      <br></br><img src="/주댕치.jpg" width="80" alt=""></img></>
   );
 }
