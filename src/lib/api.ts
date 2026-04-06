bba3ee4585e2ba8f2e11a8c93ac41b5352e49e54
성채원 <julia2003a@naver.com>
2026-04-07 08:11:35 +0900

    API 클라이언트 생성

---
+++ src/lib/api.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: { "Content-Type": "application/json" },
+});
+
+// 예시: 게시글 목록 조회
+export const fetchPosts = async () => {
+    const res = await api.get("/posts");
+    return res.data;
+};
+
+export const fetchPost = async(id: string) => {
+    const res = await api.get("/posts/${id}");
+    return res.data;
+};
+
+export const createPost = async(data: string) => {
+    const res = await api.post("/posts", data);
+    return res.data;
+};
+
+export const deletePost = async(id: string) => {
+    const res = await api.delete("/posts/${id}");
+    return res.data;
+};
+
+export const toggleLike = async(id: string) => {
+    const res = await api.patch("/posts/${id}/like");
+    return res.data;
+};
+
+export const createComment = async(id: string, data: string) => {
+    const res = await api.post("/posts/${id}/comments", data);
+    return res.data;
+};
+
+export const deleteComment = async(commentId: string) => {
+    const res = await api.delete("/comments/${commentId}");
+    return res.data;
+};
