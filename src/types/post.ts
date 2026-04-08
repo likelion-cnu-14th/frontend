f57b1f7adc1adab5518f443f7377e31cb7ba3beb
이서형 <lshlsh2523@naver.com>
2026-04-08 14:25:55 +0900

    refactor: 게시글 및 댓글 타입을 API 응답 구조에 맞게 수정

--- src/types/post.ts
+++ src/types/post.ts
@@ -5,12 +5,30 @@ export interface Comment {
   createdAt: string;
 }
 
-export interface Post {
+interface PostBase {
   id: string;
   title: string;
   content: string;
   author: string;
   createdAt: string;
   likes: number;
+}
+
+export interface PostListItem extends PostBase {
+  commentCount: number;
+}
+
+export interface PostDetail extends PostBase {
   comments: Comment[];
+}
+
+export interface CreatePostRequest {
+  title: string;
+  content: string;
+  author: string;
+}
+
+export interface CreateCommentRequest {
+  content: string;
+  author: string;
 }
