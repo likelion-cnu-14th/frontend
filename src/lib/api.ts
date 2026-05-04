1a981849af11d003c6c9f1fefd470685d835965c
성채원 <julia2003a@naver.com>
2026-05-04 19:45:00 +0900

    feat: 1-1 타입 정의

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Post, PostDetail } from "@/types/post";
-import { User, TokenResponse } from "@/types/auth";
+import { User, TokenResponse } from "@/types/post";
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
